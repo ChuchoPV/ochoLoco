bfc2c05be5786bcc24cfc12d0e15868ba8479b8a
ServioTRC <a01371719@itesm.mx>
2019-11-23 02:50:40 +0000

    random cards

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,13 +7,15 @@ function randomString() {
   return base64url(crypto.randomBytes(5));
 }
 
+const cards = ["PA", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10", "PJ", "PQ", "PK", "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK", "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJ", "DQ", "DK", "TA", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "TJ", "TQ", "TK"]
+
 exports.crearJuego = (req, res) => {
   // Creación de un documento de tipo Marca. La información se obtiene del JSON que manda el cliente.
   var game = new Game({
       id: randomString(),
       players: [],
-      deck_cards: ["PA", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10", "PJ", "PQ", "PK", "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK", "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJ", "DQ", "DK", "TA", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "TJ", "TQ", "TK"],
-      top_card: "CA"
+      deck_cards: cards,
+      top_card: cards[Math.floor((Math.random() * cards.length))]
   });
 
   // Guardar la información en la base de datos.
